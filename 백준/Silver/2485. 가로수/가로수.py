c9dbdf71a3b5f9b3cde9701ab2e16638149ef727
Sujin Kim <ksj030120@gmail.com>
2025-11-11 01:51:05 +0900

    Improve code readability and add comments
    Refactor code structure and add comments for clarity.

--- 백준/Silver/2485. 가로수/가로수.py
+++ 백준/Silver/2485. 가로수/가로수.py
@@ -1,20 +1,31 @@
+'''
+새로 정한 균일 간격 g -> 현재 간격들과 g는 배수 관계
+-> g는 현재 간격들의 공약수임
+-> 최소한의 나무를 심기 위해서 g는 현재 간격들의 최대공약수여야 함
+'''
+
 import sys
 n = int(input())
 nlist = []
 for _ in range(n):
     nlist.append(int(sys.stdin.readline().strip()))
 newlist = []
+
+# 1. 간격 리스트 생성
 for i in range(len(nlist)-1):
     newlist.append(nlist[i+1]-nlist[i])
 
+# 2. 최대공약수 함수(유클리드 호제법)
 def gcd(a,b):
     while b:
         a, b = b, a % b
     return a
 
+# 3. 여러값의 최대공약수 계산
 gcd_value = newlist[0]
 for i in range(1, len(newlist)):
     gcd_value = gcd(gcd_value, newlist[i])
 
+# 4. 필요한 나무 수 - 기존 나무 수
 ans = (nlist[-1] - nlist[0]) // gcd_value + 1 - n
 print(ans)
