db3de6023b6fae43c92873e35e62bcc697a30b59
Sujin Kim <ksj030120@gmail.com>
2025-12-09 00:19:17 +0900

    Update 한수.py

--- 백준/Silver/1065. 한수/한수.py
+++ 백준/Silver/1065. 한수/한수.py
@@ -2,28 +2,24 @@ import sys
 N_str = sys.stdin.readline().strip()
 N = int(N_str)
 
-def is_hansu(n):
-    n_str = str(n)
-    L = len(n_str)
-    
-    if L <= 2:
-        return True
-    d = int(n_str[1]) - int(n_str[0])
-
-    for i in range(1, L - 1): 
-        current_diff = int(n_str[i+1]) - int(n_str[i])
-
-        if current_diff != d:
-            return False
-    return True
-
 if N < 100:
     ans = N
 else:
     ans = 99
     
     for checknum in range(100, N + 1):
-        if is_hansu(checknum):
+        n_str = str(checknum)
+        L = len(n_str)
+        flag = 0
+        d = int(n_str[1]) - int(n_str[0])
+
+        for i in range(1, L - 1): 
+            current_diff = int(n_str[i+1]) - int(n_str[i])
+            if current_diff != d:
+                flag = 1
+                break
+
+        if flag == 0:
             ans += 1
 
 print(ans)
